2ba2ccc19a47475dfeb2df272fba22a7ea27106e
Eliezer Garbin <eliezergarbin1@gmail.com>
2022-08-02 11:07:20 -0300

    Create Create and Read CRUD

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "eliezer",
+    email: "eliezergarbin1@gmail.com",
+    celular: "0099988887",
+    cidade: "Tupã"
+}
+
+// CRUD
+const createClient = (client) => {
+    const db_client = JSON.parse(localStorage.getItem('db_client'));
+    db_client.push(client)
+    localStorage.setItem("db_client", JSON.stringify(db_client))
+}
+
+
+//eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
